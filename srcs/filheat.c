c0af5896a19b3c8d130a09b09e9ca29b628a3788
Pierre CAHIER <pcahier@e1r5p12.42.fr>
2018-11-28 19:39:38 +0100

    minor changes, no more printing of heatmap

--- srcs/filheat.c
+++ srcs/filheat.c
@@ -6,7 +6,7 @@
 /*   By: pcahier <pcahier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 16:05:59 by pcahier           #+#    #+#             */
-/*   Updated: 2018/11/19 18:17:42 by pcahier          ###   ########.fr       */
+/*   Updated: 2018/11/28 18:54:07 by pcahier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,36 +53,8 @@ static void	heat_it_up(struct s_filler *inf, int min_val)
 
 void		fil_heat(struct s_filler *inf)
 {
-	int		x;
-	int		y;
 	int		min_val;
 
 	min_val = -1;
 	heat_it_up(inf, min_val);
-	x = 0;
-	y = 0;
-	while (y < inf->lin)
-	{
-		while (x < inf->col)
-		{
-			if (inf->coord[y][x] == INTMAX)
-				ft_errprintf("LIB");
-			else if (inf->coord[y][x] == -INTMAX)
-				ft_errprintf("{black}ME!{eoc}");
-			else if (inf->coord[y][x] / 100)
-				ft_errprintf("{cyan}%3d{eoc}", inf->coord[y][x]);
-			else if (inf->coord[y][x] / 75)
-				ft_errprintf("{green}%3d{eoc}", inf->coord[y][x]);
-			else if (inf->coord[y][x] / 50)
-				ft_errprintf("{yellow}%3d{eoc}", inf->coord[y][x]);
-			else if (inf->coord[y][x] > -1)
-				ft_errprintf("{magenta}%3d{eoc}", inf->coord[y][x]);
-			else
-				ft_errprintf("{red}%3d{eoc}", inf->coord[y][x]);
-			x++;
-		}
-		ft_errprintf("\n");
-		x = 0;
-		y++;
-	}
 }
